449305b9ba2ee1aab2a07a05d6318b86732e04bf
Esha <eshagupta7510078324@gmail.com>
2023-08-14 20:14:35 +0530

    Modified the look

--- JS/worldclock.js
+++ JS/worldclock.js
@@ -1,42 +1,45 @@
 //window.alert("hii")
 /*setInterval(()=>{
-	d=new Date();
-	india.innerHTML=d.toLocaleString('en-US',{
-	timezone:'Asia/Koltata'
+  d=new Date();
+  india.innerHTML=d.toLocaleString('en-US',{
+  timezone:'Asia/Koltata'
 }).split(',')[1]
 */
 
 const usa = document.getElementById("usa");
 const india = document.getElementById("india");
 const dubai = document.getElementById("dubai");
+const china = document.getElementById("china");
+const albania = document.getElementById("albania");
+const antractica = document.getElementById("antractica");
 
-  function updateTime() {
-    const d = new Date();
+function updateTime() {
+  const d = new Date();
 
-    // Time in USA (America/New_York)
-    usa.innerHTML = d.toLocaleString("en-US", {
-      timeZone: "America/New_York",
-    }).split(",")[1];
+  // Time in USA (America/New_York)
+  usa.innerHTML = d.toLocaleString("en-US", {
+    timeZone: "America/New_York",
+  }).split(",")[1];
 
-    // Time in India (Asia/Kolkata)
-    india.innerHTML = d.toLocaleString("en-US", {
-      timeZone: "Asia/Kolkata",
-    }).split(",")[1];
+  // Time in India (Asia/Kolkata)
+  india.innerHTML = d.toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  }).split(",")[1];
 
-    // Time in Dubai (Asia/Dubai)
-    dubai.innerHTML = d.toLocaleString("en-US", {
-      timeZone: "Asia/Dubai",
-    }).split(",")[1];
-	
-	china.innerHTML = d.toLocaleString("en-US", {
-      timeZone: "Asia/Urumqi",
-    }).split(",")[1];
-	albania.innerHTML = d.toLocaleString("en-US", {
-      timeZone: "Europe/Tirane",
-    }).split(",")[1];
-	antractica.innerHTML = d.toLocaleString("en-US", {
-      timeZone: "Antarctica/DumontDUrville",
-    }).split(",")[1];
-  }
+  // Time in Dubai (Asia/Dubai)
+  dubai.innerHTML = d.toLocaleString("en-US", {
+    timeZone: "Asia/Dubai",
+  }).split(",")[1];
 
-  setInterval(updateTime,1000);
+  china.innerHTML = d.toLocaleString("en-US", {
+    timeZone: "Asia/Urumqi",
+  }).split(",")[1];
+  albania.innerHTML = d.toLocaleString("en-US", {
+    timeZone: "Europe/Tirane",
+  }).split(",")[1];
+  antractica.innerHTML = d.toLocaleString("en-US", {
+    timeZone: "Antarctica/DumontDUrville",
+  }).split(",")[1];
+}
+
+setInterval(updateTime, 1000);
